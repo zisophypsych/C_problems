ccb0fefc3f217897ba26b03d0f57ae9c60aefd34
zisophypsych <ziyaulhaqkhan60@gmail.com>
2024-01-19 09:23:49 +0530

    To add 2 numbers

--- add2num.c
+++ add2num.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
   int a,b;
-  printf("This is the program for adding two numbers!\n");
+  printf("This is the program for adding two numbers!!\n");
   printf("Enter the First Number: ");
   scanf("%d", &a);
   printf("Enter the second Number: ");
